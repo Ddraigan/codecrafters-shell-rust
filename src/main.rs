735fbf77b2302a91b336296d7d0d5c8c1511d429
Leon Jones <Leon@rnw.local>
2026-05-06 16:56:04 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use std::io::{self, Write};
 
+enum BuiltinCommand {
+    Exit,
+    Echo(String),
+}
+
+impl BuiltinCommand {
+    fn execute(command: &str) -> anyhow::Result<BuiltinCommand> {
+        let command = command.trim();
+        if let Some(args) = command.strip_prefix("echo ") {
+            return Ok(BuiltinCommand::Echo(args.into()));
+        }
+
+        if command == "exit" {
+            return Ok(BuiltinCommand::Exit);
+        }
+
+        Err(anyhow::anyhow!("{}: command not found", command))
+    }
+}
+
 fn main() {
     loop {
         print!("$ ");
@@ -7,9 +27,14 @@ fn main() {
 
         let mut command = String::default();
         io::stdin().read_line(&mut command).unwrap();
-        if command.trim() == "exit" {
-            break;
-        }
-        println!("{}: command not found", command.trim());
+        match BuiltinCommand::execute(&command) {
+            Ok(com) => match com {
+                BuiltinCommand::Exit => break,
+                BuiltinCommand::Echo(args) => println!("{}", args),
+            },
+            Err(_) => {
+                println!("{}: command not found", command.trim());
+            }
+        };
     }
 }
