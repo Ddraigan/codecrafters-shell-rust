a3656cc6f5ed1b4f23962bdd9536191cdf741594
Leon Jones <Leon@rnw.local>
2026-05-08 11:34:51 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,82 @@
 use std::io::{self, Write};
 
-enum BuiltinCommand {
-    Exit,
-    Echo(String),
-}
-
-impl BuiltinCommand {
-    fn execute(command: &str) -> anyhow::Result<BuiltinCommand> {
-        let command = command.trim();
-        if let Some(args) = command.strip_prefix("echo ") {
-            return Ok(BuiltinCommand::Echo(args.into()));
-        }
-
-        if command == "exit" {
-            return Ok(BuiltinCommand::Exit);
-        }
-
-        Err(anyhow::anyhow!("{}: command not found", command))
-    }
-}
-
 fn main() {
-    loop {
+    let mut shell = Shell::default();
+
+    while shell.state == State::Continue {
+        // Print Prompt
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command = String::default();
-        io::stdin().read_line(&mut command).unwrap();
-        match BuiltinCommand::execute(&command) {
-            Ok(com) => match com {
-                BuiltinCommand::Exit => break,
-                BuiltinCommand::Echo(args) => println!("{}", args),
-            },
-            Err(_) => {
-                println!("{}: command not found", command.trim());
+        // Read Input
+        let mut input = String::default();
+        io::stdin().read_line(&mut input).unwrap();
+
+        match BuiltinCommand::parse(&input) {
+            Ok(cmd) => {
+                shell.state = cmd.execute();
             }
+            Err(err) => {
+                eprintln!("{}", err);
+            }
+        }
+    }
+}
+
+#[derive(PartialEq, Default)]
+enum State {
+    #[default]
+    Continue,
+    Stop,
+}
+
+enum BuiltinCommand {
+    Exit,
+    Echo(String),
+    Type(String),
+    NoOp,
+}
+
+#[derive(Default)]
+struct Shell {
+    state: State,
+}
+
+impl BuiltinCommand {
+    fn execute(&self) -> State {
+        match self {
+            BuiltinCommand::Exit => State::Stop,
+            BuiltinCommand::Echo(args) => {
+                println!("{}", args);
+                State::Continue
+            }
+            BuiltinCommand::Type(_cmd) => {
+                println!("{} is a shell builtin", _cmd);
+                State::Continue
+            }
+            BuiltinCommand::NoOp => State::Continue,
+        }
+    }
+
+    fn parse(command: &str) -> anyhow::Result<BuiltinCommand> {
+        let mut parts = command.trim().split_ascii_whitespace();
+
+        let first_word = match parts.next() {
+            Some(word) => word,
+            None => return Ok(Self::NoOp),
         };
+
+        match first_word {
+            "exit" => Ok(Self::Exit),
+            "echo" => {
+                let remainder: Vec<&str> = parts.collect();
+                Ok(Self::Echo(remainder.join(" ")))
+            }
+            "type" => {
+                let arg = parts.next().unwrap_or_default();
+                Ok(Self::Type(arg.to_string()))
+            }
+            _ => Err(anyhow::anyhow!("{}: command not found", first_word)),
+        }
     }
 }
